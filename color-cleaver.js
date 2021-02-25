f85e4bc3095559f36d6e6af4c3b75720c9501e47
Timothy Negron <timothy.negron@codeimmersives.com>
2021-02-25 16:51:12 -0500

    Adds node front end for color-clear app.

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,45 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+let color1 = getInput(1);
+let color2 = getInput(2);
+
+if (color1 === undefined) {
+  console.log(
+    "\nPlease enter at least one color.\n" +
+      "A maximum of two colors can be taken as an argument.\n" +
+      "\nFor one argument only, please enter one of these colors: orange, green, purple." +
+      "\nFor two arguments, the two colors must be different and must be any of these colors: red, blue, yellow.\n"
+  );
+} else if (color2 === undefined) {
+  if (isValidSecondary(color1)) {
+    console.log(
+      "\nThese colors make the color you entered: " +
+        colorDeconstructor(color1) +
+        "\n"
+    );
+  } else {
+    console.log(
+      "\nFor one argument only, please enter one of these colors: orange, green, purple.\n"
+    );
+  }
+} else if (color1 === color2) {
+  console.log(
+    "\nYour colors must be different." +
+      "\n\nFor one argument only, please enter one of these colors: orange, green, purple." +
+      "\nFor two arguments, the two colors must be different and must be any of these colors: red, blue, yellow.\n"
+  );
+} else {
+  if (isValidPrimary(color1) && isValidPrimary(color2)) {
+    console.log(
+      "\nMixing those colors will make: " +
+        colorCombinator(color1, color2) +
+        "\n"
+    );
+  } else {
+    console.log(
+      "\nFor two arguments, the two colors must be different and must be any of these colors: red, blue, yellow.\n"
+    );
+  }
+}
--- color-combinator.js
+++ color-combinator.js
@@ -2,22 +2,27 @@
  * YOUR CODE HERE *
  ******************/
 
-
 function colorCombinator(color1, color2) {
-  if (color1 === 'red' && color2 === 'yellow' || color1 === 'yellow' && color2 === 'red') {
-    return 'orange';
-  } else if (color1 === 'red' && color2 === 'blue' || color1 === 'blue' && color2 === 'red') {
-    return 'purple';
-  } else if (color1 === 'yellow' && color2 === 'blue' || color1 === 'blue' && color2 === 'yellow') {
-    return 'green';
+  if (
+    (color1 === "red" && color2 === "yellow") ||
+    (color1 === "yellow" && color2 === "red")
+  ) {
+    return "orange";
+  } else if (
+    (color1 === "red" && color2 === "blue") ||
+    (color1 === "blue" && color2 === "red")
+  ) {
+    return "purple";
+  } else if (
+    (color1 === "yellow" && color2 === "blue") ||
+    (color1 === "blue" && color2 === "yellow")
+  ) {
+    return "green";
   }
- 
- 
- 
 }
 
 // Our setup code here. Don't touch!
-if(typeof colorCombinator === 'undefined') {
+if (typeof colorCombinator === "undefined") {
   colorCombinator = undefined;
 }
 
