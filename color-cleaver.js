1b4aa20e0ba64f05d1a3a74fb5b28cde0e9b34dd
Timothy Negron <timothy.negron@codeimmersives.com>
2021-02-26 18:24:15 -0500

    Adds functions main program in log-up.js.

--- color-cleaver.js
+++ color-cleaver.js
@@ -9,82 +9,91 @@ const getInput = require("./get-input.js");
 let color1 = getInput(1);
 let color2 = getInput(2);
 
-// Check if there is a first argument
+/* Main Program */
+
 if (color1 === undefined) {
-  // If there isn't a first argument send the user a message telling them
+  didNotEnterFirstColorMessage();
+} else if (color2 === undefined) {
+  onlyOneColorEntered(color1);
+} else if (color1 === color2) {
+  colorsAreTheSameMessage();
+} else {
+  twoColorsEntered(color1, color2);
+}
+
+/* Main Program End */
+
+/* Functions */
+
+function firstColorInvalidMessage() {
+  console.log("\nYour first color is not valid.");
+}
+
+function secondColorInvalidMessage() {
+  console.log("\nYour second color is not valid.");
+}
+
+function bothColorsInvalidMessage() {
+  console.log("\nBoth colors are not valid.");
+}
+
+function colorsAreTheSameMessage() {
+  console.log("\nYour colors must be different.");
+  helpUser();
+}
+
+function didNotEnterFirstColorMessage() {
   console.log(
-    "\nPlease enter at least one color.\n" +
-      "A maximum of two colors can be taken.\n" +
-      "\nFor one argument only, please enter one of these secondary colors: orange, green, purple." +
-      "\nFor two arguments, the two colors must be different and must be any of these primary colors: red, blue, yellow.\n"
+    "\nPlease enter at least one color." +
+      "\nA maximum of two colors can be taken."
+  );
+  helpUser();
+}
+
+function helpUser() {
+  console.log(
+    "\nFor one argument only, please enter one of these" +
+      "secondary colors: orange, green, purple." +
+      "\nFor two arguments, the two colors must be different" +
+      "and must be any of these primary colors: red, blue, yellow.\n"
   );
 }
-// If there is a first argument check if there is a second argument
-else if (color2 === undefined) {
-  // If there isn't a second argument check if the color is valid
-  if (isValidSecondary(color1)) {
-    // Send the user info about their color if it's valid
+
+function onlyOneColorEntered(firstColor) {
+  if (isValidSecondary(firstColor)) {
+    // The color is valid
     console.log(
       "\nThese colors make the color you entered: " +
-        colorDeconstructor(color1) +
+        colorDeconstructor(firstColor) +
         "\n"
     );
-  }
-  // If the color is not valid send the user a message telling them it's not
-  else {
-    console.log(
-      "\nYour color is not valid.\n\nFor one argument only, please enter one of these secondary colors: orange, green, purple.\n"
-    );
+  } else {
+    firstColorInvalidMessage();
+    helpUser();
   }
 }
-// If there are two arguments check if the colors are the same
-else if (color1 === color2) {
-  // If the colors are the same, tell the user that they must be different.
-  console.log(
-    "\nYour colors must be different." +
-      "\n\nFor one argument only, please enter one of these secondary colors: orange, green, purple." +
-      "\nFor two arguments, the two colors must be different and must be any of these primary colors: red, blue, yellow.\n"
-  );
-}
-// If all other conditions are false, then they're must be two colors
-else {
-  // Check if both colors are valid
-  if (isValidPrimary(color1) && isValidPrimary(color2)) {
-    // Send the user info about the colors if their both valid
+
+function twoColorsEntered(firstColor, secondColor) {
+  if (isValidPrimary(firstColor) && isValidPrimary(secondColor)) {
+    // Both colors are valid
     console.log(
       "\nMixing those colors will make: " +
-        colorCombinator(color1, color2) +
+        colorCombinator(firstColor, secondColor) +
         "\n"
     );
-  }
-  // Check if both are invalid
-  else if (!isValidPrimary(color1) && !isValidPrimary(color2)) {
-    // If both colors are invalid send the user a message telling them
-    console.log(
-      "\nBoth colors are not valid." +
-        "\n\nFor two arguments, the two colors must be different and must be any of these primary colors: red, blue, yellow.\n"
-    );
-  }
-  // Check if first color is invalid
-  else if (!isValidPrimary(color1)) {
-    // Tell the user the first color is invalid
-    console.log("\nYour first color is not valid.");
-    // Check if the second color is invalid
-    if (!isValidPrimary(color2)) {
-      // Tell the user if the second color is invalid
-      console.log("\nYour second color is not valid.");
+  } else if (!isValidPrimary(firstColor) && !isValidPrimary(secondColor)) {
+    bothColorsInvalidMessage();
+    helpUser();
+  } else if (!isValidPrimary(firstColor)) {
+    firstColorInvalidMessage();
+    if (!isValidPrimary(secondColor)) {
+      secondColorInvalidMessage();
     }
-    // Tell the user what are valid colors for two arguments
-    console.log(
-      "\n\nFor two arguments, the two colors must be different and must be any of these primary colors: red, blue, yellow.\n"
-    );
-  }
-  // If all other test fail then only the second color is not valid
-  else {
-    // Tell the user that the second color is not valid
-    console.log(
-      "\nYour second color is not valid." +
-        "\n\nFor two arguments, the two colors must be different and must be any of these primary colors: red, blue, yellow.\n"
-    );
+    helpUser();
+  } else {
+    secondColorInvalidMessage();
+    helpUser();
   }
 }
+
+/* Functions End */
